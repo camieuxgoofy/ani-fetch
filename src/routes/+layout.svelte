f8b99055f5b914489f9c3c836f4bee788bb34790
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-04-10 18:07:44 +0700

    minor update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav class="bg-yellow-500">
+  <a href="/">Home</a>
+  <a href="/test" data-sveltekit-prefetch>test</a>
+</nav>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
-<!-- ExampleComponent.svelte -->
 <script>
-    import { onMount } from "svelte";
-    
-    let aniData = [];
-    
-    const query = `
-      query ($page: Int) {
-        Page (page: $page, perPage: 10) {
-          media {
+  import { onMount } from "svelte";
+  import { lazyLoad } from "./lazyLoad";
+
+  let aniData = [];
+
+  const query = `
+      query ($page: Int, $score: Int) {
+        Page (page: $page, perPage: 20) {
+          media (tag_in: ["Yuri"], averageScore_greater: $score) {
             id,
             title {
               romaji,
@@ -15,51 +15,95 @@
               native
             },
             episodes,
-            genres
+            genres,
+            coverImage {
+              medium
+            },
+            isAdult,
+            averageScore,
+            meanScore,
+            tags {
+              name
+            },
           }
         }
       }`;
-    
-    const variables = { "page": 1 };
-    
-    const fetchResults = async () => {
-      const url = "https://graphql.anilist.co";
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          query: query,
-          variables: variables
-        })
-      };
-      try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        aniData = data.data.Page.media;
-      } catch (err) {
-        console.error(err);
-      }
+
+  let score = 0;
+
+  const variables = { page: 1, score };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    variables.score = score;
+    fetchResults();
+  };
+
+  const fetchResults = async () => {
+    const url = "https://graphql.anilist.co";
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
     };
-    
-    onMount(() => {
-      fetchResults();
-    });
-  </script>
-    
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      aniData = data.data.Page.media;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  onMount(() => {
+    fetchResults();
+  });
+</script>
+
+<section>
+  <form on:submit={handleSubmit}>
+    <label for="score-input">Minimum score:</label>
+    <input
+      type="number"
+      id="score-input"
+      name="score"
+      bind:value={score}
+      min="0"
+      max="100"
+    />
+    <button type="submit">Search</button>
+  </form>
+  {#each aniData as anime}
   <div>
-    <ul>
-      {#each aniData as anime}
-        <li>
-          <h3>{anime.title.romaji == null ? '' : anime.title.romaji}</h3>
-          <h3>{anime.title.english == null ? '' : anime.title.english}</h3>
-          <h3>{anime.title.native == null ? '' : anime.title.native}</h3>
-          <p>Episodes: {anime.episodes}</p>
-          <p>Genres: {anime.genres}</p>
-        </li>
-      {/each}
-    </ul>
+    <h3>{anime.title.romaji == null ? "" : anime.title.romaji}</h3>
+    <h3>{anime.title.english == null ? "" : anime.title.english}</h3>
+    <h3>{anime.title.native == null ? "" : anime.title.native}</h3>
+    <h1>
+      {anime.title.romaji == anime.title.english && anime.title.native
+        ? ""
+        : anime.title.romaji}
+    </h1>
+    <p>Episodes: {anime.episodes}</p>
+    <p>Genres: {anime.genres}</p>
+    <img use:lazyLoad={anime.coverImage.medium} alt={anime.title.romaji} />
+    <p>
+      {anime.averageScore}
+    </p>
+    <p>{anime.meanScore}</p>
+    <span>{anime.tags.name}</span>
   </div>
-  
+{/each}
+</section>
+
+<style>
+  img {
+    opacity: 0;
+    transition: all 1s ease;
+  }
+</style>
